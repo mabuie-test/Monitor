0161ec24d2619769a19703721e913c3cf7049a78
JorgeMabuie <jorgemaabuie@gmail.com>
2025-08-31 08:45:25 +0200

    Update AppUsage.js

--- models/AppUsage.js
+++ models/AppUsage.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 const appUsageSchema = new mongoose.Schema({
-    deviceId: String,
-    packageName: String,
-    totalTime: Number,
-    lastTimeUsed: Date,
-    createdAt: { type: Date, default: Date.now }
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', index: true },
+  deviceId: String,
+  packageName: String,
+  totalTime: Number,
+  lastTimeUsed: Date,
+  createdAt: { type: Date, default: Date.now }
 });
 module.exports = mongoose.model('AppUsage', appUsageSchema);
