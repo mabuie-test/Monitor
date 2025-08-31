3502d00d12434e4e77c0ded0d709d069dd9539e2
JorgeMabuie <jorgemaabuie@gmail.com>
2025-08-31 08:41:40 +0200

    Update Location.js

--- models/Location.js
+++ models/Location.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const locationSchema = new mongoose.Schema({
-    deviceId: String,
-    lat: Number,
-    lon: Number,
-    accuracy: Number,
-    timestamp: Date,
-    createdAt: { type: Date, default: Date.now }
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', index: true },
+  deviceId: String,
+  lat: Number,
+  lon: Number,
+  accuracy: Number,
+  timestamp: Date,
+  createdAt: { type: Date, default: Date.now }
 });
 module.exports = mongoose.model('Location', locationSchema);
