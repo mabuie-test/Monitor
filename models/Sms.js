d77636e365e34e937ca013fcdec344b241c47053
JorgeMabuie <jorgemaabuie@gmail.com>
2025-08-31 08:39:20 +0200

    Update Sms.js

--- models/Sms.js
+++ models/Sms.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 const smsSchema = new mongoose.Schema({
-    deviceId: String,
-    sender: String,
-    message: String,
-    timestamp: Date,
-    createdAt: { type: Date, default: Date.now }
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', index: true },
+  deviceId: String,
+  sender: String,
+  message: String,
+  timestamp: Date,
+  createdAt: { type: Date, default: Date.now }
 });
 module.exports = mongoose.model('Sms', smsSchema);
