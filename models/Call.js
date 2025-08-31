ae7769759d459ab17a6f2a73e5ca6618118a1d7b
JorgeMabuie <jorgemaabuie@gmail.com>
2025-08-31 08:41:11 +0200

    Update Call.js

--- models/Call.js
+++ models/Call.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const callSchema = new mongoose.Schema({
-    deviceId: String,
-    number: String,
-    type: String,
-    state: String,
-    timestamp: Date,
-    createdAt: { type: Date, default: Date.now }
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', index: true },
+  deviceId: String,
+  number: String,
+  type: String,
+  state: String,
+  timestamp: Date,
+  createdAt: { type: Date, default: Date.now }
 });
 module.exports = mongoose.model('Call', callSchema);
