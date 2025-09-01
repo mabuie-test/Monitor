a4e38e7012b99c473a586237ca2a73d22ee7361f
JorgeMabuie <jorgemaabuie@gmail.com>
2025-09-01 10:24:34 +0200

    Delete models directory

--- models/Consent.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const consentSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', index: true },
-  deviceId: String,
-  consent: Boolean,
-  timestamp: Date,
-  createdAt: { type: Date, default: Date.now }
-});
-module.exports = mongoose.model('Consent', consentSchema);
