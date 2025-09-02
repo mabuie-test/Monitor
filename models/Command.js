c32c618d53feaada24c2e201653c46b0d3577bc3
JorgeMabuie <jorgemaabuie@gmail.com>
2025-09-02 18:54:23 +0200

    Create Command.js

---
+++ models/Command.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CommandSchema = new Schema({
+  targetDeviceId: { type: String, required: true, index: true },
+  targetUser: { type: Schema.Types.ObjectId, ref: 'User' },
+  command: { type: String, required: true },
+  params: { type: Schema.Types.Mixed },
+  status: { type: String, enum: ['pending','done'], default: 'pending' },
+  createdAt: { type: Date, default: Date.now },
+  doneAt: Date
+});
+
+module.exports = mongoose.model('Command', CommandSchema);
