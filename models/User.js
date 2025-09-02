dc7eb05e15ef934303de95b9db359562dce23d68
JorgeMabuie <jorgemaabuie@gmail.com>
2025-09-02 23:33:08 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,10 +1,18 @@
+// models/User.js
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   username: { type: String, required: true, index: true, unique: true },
   passwordHash: { type: String, required: true },
+  phone: { type: String, default: null }, // opcional
   createdAt: { type: Date, default: Date.now }
 });
+
+// cria índice parcial para phone (somente quando phone existe e não é null)
+UserSchema.index(
+  { phone: 1 },
+  { unique: true, partialFilterExpression: { phone: { $exists: true, $ne: null } } }
+);
 
 module.exports = mongoose.model('User', UserSchema);
