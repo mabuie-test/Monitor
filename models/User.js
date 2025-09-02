271f3db20f3afdfd34ffa908c228c7029536e894
JorgeMabuie <jorgemaabuie@gmail.com>
2025-09-02 22:59:05 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new Schema({
   username: { type: String, required: true, index: true, unique: true },
   passwordHash: { type: String, required: true },
   createdAt: { type: Date, default: Date.now }
