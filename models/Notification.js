55867caf9432e2f7e2acb5e94f7433418eb905af
JorgeMabuie <jorgemaabuie@gmail.com>
2025-09-03 08:31:49 +0200

    Update Notification.js

--- models/Notification.js
+++ models/Notification.js
@@ -2,13 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const NotificationSchema = new Schema({
-  deviceId: String,
-  user: { type: Schema.Types.ObjectId, ref: 'User' },
-  packageName: String,
-  title: String,
-  message: String,
-  timestamp: Date,
-  createdAt: { type: Date, default: Date.now }
+  deviceId: { type: String, required: true, index: true },
+  user: { type: Schema.Types.ObjectId, ref: 'User', index: true },
+  packageName: { type: String },
+  message: { type: String },
+  timestamp: { type: Date, default: Date.now }
 });
-
 module.exports = mongoose.model('Notification', NotificationSchema);
