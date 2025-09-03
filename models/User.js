48302fa9b33de99cc98047340867c866b0445948
JorgeMabuie <jorgemaabuie@gmail.com>
2025-09-03 08:20:34 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ const UserSchema = new Schema({
   username: { type: String, required: true, unique: true },
   passwordHash: { type: String, required: true },
   phone: { type: String, default: null },
-  role: { type: String, default: 'user' }, // 'user' ou 'admin'
+  role: { type: String, default: 'user' },
   createdAt: { type: Date, default: Date.now }
 });
 
